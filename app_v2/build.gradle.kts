133ac6b0a43f8a2d3fcfa0016fd5f84772580dc9
Serhii Zhyhunenko <serzhyale@gmail.com>
2025-11-11 18:31:18 +0100

    Fix hardcoded 'чередь' text to use localized string resource

--- app_v2/build.gradle.kts
+++ app_v2/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.sza.fastmediasorter_v2"
         minSdk = 28
         targetSdk = 34
-        versionCode = 11111826
-        versionName = "2.0.0-build2511111826"
+        versionCode = 11111830
+        versionName = "2.0.0-build2511111830"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
