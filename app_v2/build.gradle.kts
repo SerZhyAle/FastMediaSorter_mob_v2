71abdd6003e5b6619fc97edefdbfdc24b439ee64
Serhii Zhyhunenko <serzhyale@gmail.com>
2025-11-17 11:23:28 +0100

    BUILD: Version 2.0.2511171122 - Pre-OneDrive implementation checkpoint

--- app_v2/build.gradle.kts
+++ app_v2/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.sza.fastmediasorter_v2"
         minSdk = 28
         targetSdk = 34
-        versionCode = 11171119
-        versionName = "2.0.0-build2511171119"
+        versionCode = 11171122
+        versionName = "2.0.0-build2511171122"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
