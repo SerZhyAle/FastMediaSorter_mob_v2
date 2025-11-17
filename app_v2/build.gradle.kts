19f01404bc644cf4e68653883fa76b3e870847b8
Serhii Zhyhunenko <serzhyale@gmail.com>
2025-11-17 11:39:29 +0100

    BUILD: Version 2.0.2511171138 - Clean build after reverting OneDrive implementation (SDK compatibility issues)

--- app_v2/build.gradle.kts
+++ app_v2/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.sza.fastmediasorter_v2"
         minSdk = 28
         targetSdk = 34
-        versionCode = 11171122
-        versionName = "2.0.0-build2511171122"
+        versionCode = 11171138
+        versionName = "2.0.0-build2511171138"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
