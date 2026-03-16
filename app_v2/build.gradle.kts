09ce3940fd3a207684b2bf3c2e69e55272163e40
Serhii Zhyhunenko <serzh@mark.com>
2026-03-16 23:14:54 +0100

    2603162314

--- app_v2/build.gradle.kts
+++ app_v2/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         // versionName format: Y.YM.MDDH.Hmm (e.g., 2.62.0501.151 for 2026/02/05 01:51)
         // versionCode format: YYMMDDHHm (e.g., 260205015 for 2026/02/05 01:51)
         // Note: YYMMDDHHmm overflows Int32, using first digit of minutes only
-        versionCode = 260316184
-        versionName = "2.60.3161.848"
+        versionCode = 260316231
+        versionName = "2.60.3162.312"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
